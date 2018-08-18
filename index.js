ed57ef5049a4c984d8909141515c065640568404
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-08-18 15:03:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-function BoardMember (name, homeState, training){
-  this.name = name;
-  this.homeState = homeState;
-  this.training = training;
+function BoardMember(name, homeState, training){
+  this.name = name
+  this.homeState = homeState
+  this.training = training
 
   this.veto = function(){
-    return "No, I must disagree";
+    return 'No, I must disagree'
   }
-
-  this.approve = function (){
-    return "I like to help people";
+  this.approve = function(){
+    return 'You can do that!'
   }
 
   this.doCharity = function(){
-    return "I like to help people";
+    return "I like to help people."
   }
 
-  this.releasePressStatement = function (){
-    return "You will see great things from Scuber";
+  this.releasePressStatement = function(){
+    return "You will see great things from Scuber."
   }
 
-  this.sayHi(){
-    return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}`;
+  this.sayHi = function(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
